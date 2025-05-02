5ed87a0667d4e7faa189d4cf87ac7aa935f21b6b
Katia <katia.lovchinskaya@gmail.com>
2025-05-02 16:08:48 +0300

    not work

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,11 @@
-export const PATH_DB =
+
+import { readFile } from 'node:fs';
+
+export const PATH_DB = readFile('./db.json', {encoding: "utf-8"}, (err, data) => {
+ if (err){
+    throw err;
+ }
+ console.log(data);
+ return data;
+});
+console.log("hi y");
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { PATH_DB } from "./constants/contacts";
+
+const test = PATH_DB();
+
+console.log(test);
