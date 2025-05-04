c3e773c76b23604af0ff3994496bb940b59ac73f
Katia <katia.lovchinskaya@gmail.com>
2025-05-04 11:59:08 +0300

    fix

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,12 +1,10 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import * as fs from 'node:fs/promises';
+
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
-export const PATH_DB = path.join(dirname, '../db/db.txt');
-console.log("works");
+export const PATH_DB = path.join(dirname, '../db/db.json');
 
-console.log(fs.readFile(PATH_DB, { encoding: 'utf-8'}));
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import { PATH_DB } from "./constants/contacts.js";
 
-const test = PATH_DB;
-
-console.log(`"test" : ${test.data}`);
